250447f2848785d11cfdb517630a4dca0198756d
jongwonpc <c3392302@naver.com>
2023-04-17 23:32:57 +0900

    fix : 게임 실패 시 입력 받는 기능 버그 픽스

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,11 +1,8 @@
 package bridge.controller;
 
 import bridge.BridgeGame;
-import bridge.BridgeMaker;
 import bridge.service.BridgeGameService;
-import bridge.view.InputView;
 import bridge.view.OutputView;
-import java.util.Arrays;
 import java.util.List;
 
 public class BridgeController {
@@ -13,28 +10,42 @@ public class BridgeController {
     InputController inputView = new InputController();
     OutputView outputView = new OutputView();
     BridgeGameService bridgeGameService = new BridgeGameService();
+    List<List<String>> progress = bridgeGameService.initGameProgress();
 
-    public void run(){
-        int size = inputView.inputSize();
-        List<String> bridge = bridgeGameService.initBridge(size);
-        BridgeGame bridgeGame = new BridgeGame(bridge);
-        List<List<String>> gameProgress = bridgeGameService.initGameProgress();
-        while(checkX(gameProgress) && gameProgress.get(0).size() <bridge.size()){
-            String input = inputView.readMoving();
-            outputView.printMap(bridgeGame,input,gameProgress);
-        }
-        if(!checkX(gameProgress)){
-            String input = inputView.readGameCommand();
-        }
-        System.out.println("최종 게임 결과");
-        outputView.printMapNoInput(gameProgress);
-    }
+   public void run() {
+       int size = inputView.inputSize();
+       List<String> bridge = bridgeGameService.initBridge(size);
+       BridgeGame bridgeGame = new BridgeGame(bridge);
+       repeatMoving(size,bridgeGame);
+       while(!checkX(progress)){
+           String str = inputView.readGameCommand();
+           if(str.equals("R")){
+               progress = bridgeGameService.initGameProgress();
+               repeatMoving(size,bridgeGame);
+           }
+           if(str.equals("Q")){
+               break;
+           }
+       }
+   }
 
+   private List<List<String>> repeatMoving(int size,BridgeGame bridgeGame){
+       while (checkXAndSize(size, progress)) {
+           String input = inputView.readMoving();
+           outputView.printMap(bridgeGame, input, progress);
+       }
+       return progress;
+   }
+
+   private boolean checkXAndSize(int size, List<List<String>> progress){
+       return checkX(progress) && progress.get(0).size()<size;
+   }
+
+    //x가 하나도 없을시에만 true
     private boolean checkX(List<List<String>> gameProgress){
         boolean flag1 = gameProgress.get(0).stream().allMatch(str -> !str.contains("X"));
         boolean flag2 = gameProgress.get(1).stream().allMatch(str -> !str.contains("X"));
         return flag1 && flag2;
     }
-
 
 }
--- src/main/java/bridge/service/BridgeGameService.java
+++ src/main/java/bridge/service/BridgeGameService.java
@@ -7,24 +7,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeGameService {
-    private final BridgeMaker bridgeMaker;
 
-    public BridgeGameService(){
+    private final BridgeMaker bridgeMaker;
+    private int count = 0;
+    public BridgeGameService() {
         bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
-    public List<String> initBridge(int size){
+    public List<String> initBridge(int size) {
         return bridgeMaker.makeBridge(size);
     }
-    public List<List<String>> initGameProgress(){
+
+    public List<List<String>> initGameProgress() {
+        count++;
         List<List<String>> gameProgress = new ArrayList<>();
         gameProgress.add(new ArrayList<>());
         gameProgress.add(new ArrayList<>());
         return gameProgress;
     }
 
-    public List<List<String>> moveService(String input, List<List<String>> gameProgress,BridgeGame bridgeGame){
-        bridgeGame.move(input,gameProgress);
+    public List<List<String>> moveService(String input, List<List<String>> gameProgress, BridgeGame bridgeGame) {
+        bridgeGame.move(input, gameProgress);
         return gameProgress;
     }
+
+    public int getCount(){
+        return count;
+    }
 }
+
